0f24e03e2d102b5984faa05fd76a3107fec029d0
Keaton Burleson <keaton.burleson@me.com>
2021-11-10 13:14:18 -0600

    Tries yet again to work with Redis/Heroku

--- src/config/cache-config.service.ts
+++ src/config/cache-config.service.ts
@@ -11,15 +11,11 @@ const YEAR_CACHE_TTL = 60 * 60 * 24 * 365;
 @Injectable()
 export class CacheConfigService implements CacheOptionsFactory {
   private readonly useRedis: boolean;
-  private readonly redisHost: string;
-  private readonly redisPort: number;
-  private readonly redisAuthPassword: string;
+  private readonly redisURL: string;
 
   constructor(private configService: ConfigService) {
     this.useRedis = configService.get('NODE_ENV') !== 'development'; // Only use redis on non-dev environments
-    this.redisHost = configService.get('REDIS_HOST') || 'localhost';
-    this.redisPort = configService.get('REDIS_PORT') || 6379;
-    this.redisAuthPassword = configService.get('REDIS_AUTH_PASS') || null;
+    this.redisURL = configService.get('REDIS_URL') || 'localhost';
   }
 
   createCacheOptions(): CacheModuleOptions {
@@ -31,9 +27,7 @@ export class CacheConfigService implements CacheOptionsFactory {
       return {
         ...baseOptions,
         store: redisStore,
-        host: this.redisHost,
-        port: this.redisPort,
-        auth_pass: this.redisAuthPassword,
+        url: this.redisURL,
       };
     }
 
