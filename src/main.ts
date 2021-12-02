776d275ac5ff2d5fa1d6633b78ebbdd936c16457
Keaton Burleson <keaton.burleson@me.com>
2021-12-02 14:34:14 -0600

    Fixes CORS issues

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    exposedHeaders: 'Content-Type, Content-Disposition, Attachment-Filename',
+  });
 
   const config = new DocumentBuilder()
     .setTitle('CIS API')
@@ -28,4 +32,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap().then();
